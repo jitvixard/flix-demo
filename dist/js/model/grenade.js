e76aae4fcfad630b3046e04f54c648765a787bc2
Samuel <samuel@morgan.ooo>
2021-04-08 20:35:12 +0100

    changed public to dist

---
+++ dist/js/model/grenade.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Grenade = void 0;
+var Grenade = /** @class */ (function () {
+    function Grenade() {
+        this.id = 'grenade';
+        this.amount = 1;
+        this.pathToAsset = 'Assets/Items/Grenade.png';
+    }
+    return Grenade;
+}());
+exports.Grenade = Grenade;
---
+++ dist/js/model/item.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AbstractItem = void 0;
+var AbstractItem = /** @class */ (function () {
+    function AbstractItem(amount) {
+        var _this = this;
+        this.getPath = function () { return '../Assets/icons/' + _this.id + '.png'; };
+        this.amount = amount;
+    }
+    return AbstractItem;
+}());
+exports.AbstractItem = AbstractItem;
